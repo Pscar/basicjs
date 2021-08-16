312c1b478bf980f54054f3414884ad33f7ab66f3
pscar <pangza115@hotmail.com>
2021-08-16 14:51:03 +0700

    message

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-// const getArray = () => {
-//   let arrayNumber = [1, 2, 3, 4, 5];
-//   console.log(arrayNumber);
-//   let isPrime = true;
+const getArray = () => {
+  let arrayNumber = [1, 2, 3, 4, 5];
+  console.log(arrayNumber);
+  let isPrime = true;
 
-//   const map1 = arrayNumber.map((number) => {
-//     let a = 0;
-//     for (let i = 2; i < number; i++) {
-//       if (number % i == 0) {
-//         isPrime = false;
-//         break;
-//       }
-//     }
-//     if (isPrime) {
-//       a += 5;
-//     } else if (number % 2 === 0) {
-//       a += 1;
-//     } else if (number % 2 !== 0) {
-//       a += 3;
-//     }
-//     return a;
-//   });
-//   console.log(map1);
-//   const sum = map1.reduce((previous, current) => previous + current, 0);
-//   console.log(sum);
-// };
-// getArray();
+  const map1 = arrayNumber.map((number) => {
+    let a = 0;
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      a += 5;
+    } else if (number % 2 === 0) {
+      a += 1;
+    } else if (number % 2 !== 0) {
+      a += 3;
+    }
+    return a;
+  });
+  console.log(map1);
+  const sum = map1.reduce((previous, current) => previous + current, 0);
+  console.log(sum);
+};
+getArray();
 
-// const sumArray = () => {
-//   let arrayNumber = [1, 2, 3, 4, 5];
-//   let sumEvens = [];
-//   let sumOdds = [];
+const sumArray = () => {
+  let arrayNumber = [1, 2, 3, 4, 5];
+  let sumEvens = [];
+  let sumOdds = [];
 
-//   const map2 = arrayNumber.map((number) => {
-//     if (number % 2 === 0) {
-//       sumEvens.push(number);
-//       return sumEvens;
-//     } else {
-//       sumOdds.push(number);
-//       return sumOdds;
-//     }
-//   });
+  const map2 = arrayNumber.map((number) => {
+    if (number % 2 === 0) {
+      sumEvens.push(number);
+      return sumEvens;
+    } else {
+      sumOdds.push(number);
+      return sumOdds;
+    }
+  });
 
-//   const a = sumEvens.reduce((previous, current) => previous + current, 0);
-//   const b = sumOdds.reduce((previous, current) => previous + current, 0);
-//   const c = a - b;
+  const a = sumEvens.reduce((previous, current) => previous + current, 0);
+  const b = sumOdds.reduce((previous, current) => previous + current, 0);
+  const c = a - b;
 
-//   console.log([
-//     { lable: "a", sum: a },
-//     { lable: "b", sum: b },
-//     { lable: "c", sum: c },
-//   ]);
-// };
-// sumArray();
+  console.log([
+    { lable: "a", sum: a },
+    { lable: "b", sum: b },
+    { lable: "c", sum: c },
+  ]);
+};
+sumArray();
 
 const sortArray = () => {
   let arrayNumber = [5, 4, 3, 2, 1];
